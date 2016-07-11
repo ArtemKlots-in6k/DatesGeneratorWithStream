7ddcc9becec8a90fafb301c64231492f004a8278
Biosss <bios_ss@mail.ru>
2016-07-12 01:58:44 +0300

    Inliny function refactoring

--- src/DateGenerator.java
+++ src/DateGenerator.java
@@ -14,18 +14,16 @@ public class DateGenerator {
     private static int LAST_POSSIBLE_WEEKEND_ON_BEGINNING_OF_MONTH = 7;
     ArrayList<LocalDate> generatedDates;
 
-    Function<Integer, Integer> limitCounter = (Integer year) -> {
-        int limit = 0;
-        for (int i = 1; i <= 12; i++) {
-            limit += LocalDate.of(year, i, 1).with(TemporalAdjusters.lastDayOfMonth()).getDayOfMonth();
-        }
-        return limit;
-    };
-
 
     public List<LocalDate> generate(int year) {
         List<LocalDate> weekends = Stream.iterate(LocalDate.of(year, 1, 1), n -> n.plusDays(1))
-                .limit(limitCounter.apply(year))
+                .limit(((Function<Integer, Integer>) (Integer yearArgument) -> {
+                    int limit = 0;
+                    for (int i = 1; i <= 12; i++) {
+                        limit += LocalDate.of(yearArgument, i, 1).with(TemporalAdjusters.lastDayOfMonth()).getDayOfMonth();
+                    }
+                    return limit;
+                }).apply(year))
                 .filter(d -> d.getDayOfWeek().getValue() >= 6)
                 .filter(d -> (d.getDayOfMonth() <= LAST_POSSIBLE_WEEKEND_ON_BEGINNING_OF_MONTH
                         || d.getDayOfMonth() >= FIRST_POSSIBLE_WEEKEND_IN_END_OF_MONTH))
