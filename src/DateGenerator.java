8e365a05002098cd03a50db7b5f1c4e41e7537da
Artem Klots <ArtemKl@interlink-ua.com>
2016-07-12 12:41:53 +0300

    Worked version

--- src/DateGenerator.java
+++ src/DateGenerator.java
@@ -1,8 +1,13 @@
 import java.time.LocalDate;
+import java.time.Month;
+import java.time.Year;
 import java.time.temporal.TemporalAdjusters;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 import java.util.function.Function;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -10,25 +15,27 @@ import java.util.stream.Stream;
  * Created by Владелец on 11.07.2016.
  */
 public class DateGenerator {
-    private static int FIRST_POSSIBLE_WEEKEND_IN_END_OF_MONTH = 25;
     private static int LAST_POSSIBLE_WEEKEND_ON_BEGINNING_OF_MONTH = 7;
-    ArrayList<LocalDate> generatedDates;
 
 
-    public List<LocalDate> generate(int year) {
-        List<LocalDate> weekends = Stream.iterate(LocalDate.of(year, 1, 1), n -> n.plusDays(1))
-                .limit(((Function<Integer, Integer>) (Integer yearArgument) -> {
-                    int limit = 0;
-                    for (int i = 1; i <= 12; i++) {
-                        limit += LocalDate.of(yearArgument, i, 1).with(TemporalAdjusters.lastDayOfMonth()).getDayOfMonth();
-                    }
-                    return limit;
-                }).apply(year))
+    public Stream<LocalDate> generate(int year) {
+        return Stream.iterate(LocalDate.of(year, 1, 1), n -> n.plusDays(1))
+                .limit(Year.of(year).length());
+    }
+
+
+    public String print(Stream<LocalDate> inputStream) {
+        Map<Month, List<LocalDate>> resultsTable = inputStream
                 .filter(d -> d.getDayOfWeek().getValue() >= 6)
                 .filter(d -> (d.getDayOfMonth() <= LAST_POSSIBLE_WEEKEND_ON_BEGINNING_OF_MONTH
-                        || d.getDayOfMonth() >= FIRST_POSSIBLE_WEEKEND_IN_END_OF_MONTH))
-                .collect(Collectors.toList());
-        System.out.println(weekends);
-        return weekends;
+                        || d.getDayOfMonth() >= Month.valueOf(d.getMonth().toString()).maxLength() - 6))
+                .collect(Collectors.groupingBy(LocalDate::getMonth));
+
+        String result = "";
+        for (int i = 1; i <= 12; i++) {
+            result += String.format("%1$-10s", Month.of(i).toString()) + resultsTable.get(Month.of(i)) + "\n";
+        }
+
+        return result;
     }
 }
--- tests/GeneratorTest.java
+++ tests/GeneratorTest.java
@@ -2,9 +2,12 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.time.LocalDate;
-import java.util.List;
+import java.util.stream.Stream;
 
-import static org.hamcrest.CoreMatchers.is;
+import static java.lang.Math.toIntExact;
+
+
+import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.assertThat;
 
 /**
@@ -12,15 +15,89 @@ import static org.junit.Assert.assertThat;
  */
 public class GeneratorTest {
     private DateGenerator dateGenerator;
+    private final String NEW_LINE_SYMBOL = "\n";
 
     @Before
-    public void before(){
+    public void before() {
         dateGenerator = new DateGenerator();
     }
 
     @Test
-    public void generatorTest() throws Exception {
-        List<LocalDate> generatedDates = dateGenerator.generate(2016);
-        assertThat(generatedDates.size(), is(46));
+    public void generator() throws Exception {
+        assertThat(toIntExact(dateGenerator.generate(2016).count()), is(366));
+    }
+
+    @Test
+    public void findWeekend() throws Exception {
+        Stream<LocalDate> stream = Stream.of(LocalDate.of(2016, 7, 30));
+        String expectedString = "JULY      [2016-07-30]";
+        assertThat(dateGenerator.print(stream), containsString(expectedString));
+    }
+
+
+    @Test
+    public void findFirstWeekendInMonth() throws Exception {
+        Stream<LocalDate> stream = Stream.of(LocalDate.of(2016, 1, 2));
+        String expectedString = "JANUARY   [2016-01-02]";
+        assertThat(dateGenerator.print(stream), containsString(expectedString));
+    }
+
+    @Test
+    public void findLastWeekendInMonth() throws Exception {
+        Stream<LocalDate> stream = Stream.of(LocalDate.of(2016, 1, 31));
+        String expectedString = "[2016-01-31]";
+        assertThat(dateGenerator.print(stream), containsString(expectedString));
+    }
+
+    @Test
+    public void isWeekdayInResult() throws Exception {
+        Stream<LocalDate> stream = Stream.of(LocalDate.of(2016, 1, 1));
+        String outputString = dateGenerator.print(stream);
+        String expectedString = "[2016-01-1]";
+        assertThat(outputString, not(outputString.contains(expectedString)));
+    }
+
+    @Test
+    public void rightMonthOutput() throws Exception {
+        Stream<LocalDate> stream = Stream.of(LocalDate.of(2016, 1, 2),
+                LocalDate.of(2016, 1, 3),
+                LocalDate.of(2016, 1, 15),
+                LocalDate.of(2016, 1, 30),
+                LocalDate.of(2016, 1, 31));
+        assertThat(dateGenerator.print(stream),
+                containsString("JANUARY   [2016-01-02, 2016-01-03, 2016-01-30, 2016-01-31]" + NEW_LINE_SYMBOL));
+    }
+
+    @Test
+    public void isAllMonthInTableFormat() throws Exception {
+        Stream<LocalDate> stream = Stream.of(LocalDate.of(2016, 1, 1));
+        String[] outputString = dateGenerator.print(stream).split(NEW_LINE_SYMBOL);
+
+        assertThat(outputString.length, is(12));
+    }
+
+    @Test
+    public void allMonthPrinting() throws Exception {
+        Stream<LocalDate> stream = Stream.of(LocalDate.of(2016, 1, 1));
+        String outputString = dateGenerator.print(stream);
+
+        assertThat(outputString, both(containsString("JANUARY"))
+                .and(containsString("FEBRUARY"))
+                .and(containsString("MARCH"))
+                .and(containsString("APRIL"))
+                .and(containsString("MAY"))
+                .and(containsString("JUNE"))
+                .and(containsString("JULY"))
+                .and(containsString("AUGUST"))
+                .and(containsString("SEPTEMBER"))
+                .and(containsString("OCTOBER"))
+                .and(containsString("NOVEMBER"))
+                .and(containsString("DECEMBER")));
+    }
+    @Test
+    public void findWeekend1() throws Exception {
+        Stream<LocalDate> stream = Stream.of(LocalDate.of(2016, 7, 30));
+        String expectedString = "JULY      [2016-07-30]";
+        assertThat(dateGenerator.print(stream), containsString(expectedString));
     }
 }
